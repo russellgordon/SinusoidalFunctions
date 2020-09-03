959d10322aae04619fedb4410b32cc3477aeb54e
Russell Gordon <rgordon@lcs.on.ca>
2020-09-03 17:49:46 -0400

    Initial Commit

---
+++ Shared/SinusoidalFunctionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinusoidalFunctionsApp.swift
+//  Shared
+//
+//  Created by Russell Gordon on 2020-09-03.
+//
+
+import SwiftUI
+
+@main
+struct SinusoidalFunctionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
