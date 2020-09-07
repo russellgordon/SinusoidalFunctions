05c4c468999eb0585ee29656f7c44b467d1de047
Russell Gordon <github@russellgordon.ca>
2020-09-06 20:22:54 -0400

    Connected the Help command to https://www.russellgordon.ca/apps#periodically

--- Shared/SinusoidalFunctionsApp.swift
+++ Shared/SinusoidalFunctionsApp.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 @main
 struct SinusoidalFunctionsApp: App {
+    
+    @Environment(\.openURL) var openURL
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }.commands {
+            
+            // Wire up the Help menu
+            // See: https://itnext.io/swiftui-keyboard-shortcuts-and-menu-bar-be22abbb3791
+            CommandGroup(replacing: CommandGroupPlacement.help) {
+
+                Button(action: {
+                    openURL(URL(string: "https://www.russellgordon.ca/apps#periodically")!)
+                }) {
+                    Text("Periodically Help")
+                }
+                
+            }
         }
     }
 }
